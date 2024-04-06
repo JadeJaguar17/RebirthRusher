9d50f2d67649b8ba357e5a7042aae7cb75363b19
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-06 19:49:48 -0400

    Format links commands

--- commands/links/invite.js
+++ commands/links/invite.js
@@ -2,34 +2,34 @@ const MessageEmbed = require("../../system/MessageEmbed");
 const { RBR } = require("../../config/embedColors.json");
 const { invite } = require("../../config/links.json");
 
-module.exports = {
-    name: "invite",
-    description: "Sends the bot invite link",
-    syntax: "`/invite`",
-    execute: async function (message) {
-        const inviteEmbed = new MessageEmbed()
-            .setColor(RBR)
-            .setAuthor(bot.user.username, bot.user.avatarURL)
-            .setTitle("Invite Me!")
-            .setThumbnail("https://i.imgur.com/HRIQMyF.png")
-            .setDescription(`Want to use me in your server? Invite me!`);
+module.exports.name = "invite"
+module.exports.description = "Sends the bot invite link"
+module.exports.syntax = "`/invite`"
 
-        return {
-            embeds: [inviteEmbed],
-            components: [
-                {
-                    type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            style: 5,
-                            label: "Invite",
-                            url: invite
-                        }
-                    ]
-                }
-            ]
-        };
-    },
-    options: []
+module.exports.execute = async function (message) {
+    const inviteEmbed = new MessageEmbed()
+        .setColor(RBR)
+        .setAuthor(bot.user.username, bot.user.avatarURL)
+        .setTitle("Invite Me!")
+        .setThumbnail("https://i.imgur.com/HRIQMyF.png")
+        .setDescription(`Want to use me in your server? Invite me!`);
+
+    return {
+        embeds: [inviteEmbed],
+        components: [
+            {
+                type: 1,
+                components: [
+                    {
+                        type: 2,
+                        style: 5,
+                        label: "Invite",
+                        url: invite
+                    }
+                ]
+            }
+        ]
+    };
 }
+
+module.exports.options = []
