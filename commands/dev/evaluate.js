1e307e62cda103280c044da778a9153832ceb0df
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-06 19:35:16 -0400

    Format dev commands

--- commands/dev/evaluate.js
+++ commands/dev/evaluate.js
@@ -1,29 +1,29 @@
 const UserDB = require("../../database/controllers/userController");
 const { dev } = require("../../config/discordIds.json");
 
-module.exports = {
-    name: "evaluate",
-    description: "Lets developers directly evaluate code with Discord messages\n*(dev only)*",
-    syntax: "`/evaluate [code]`",
-    hidden: true,
-    execute: async function (interaction) {
-        if (interaction.member.user.id === dev) {
-            try {
-                const user = await UserDB.getUserById(interaction.member.user.id);
-                const result = eval(interaction.data.options[0].value);
+module.exports.name = "evaluate"
+module.exports.description = "Lets developers directly evaluate code with Discord messages\n*(dev only)*"
+module.exports.syntax = "`/evaluate [code]`"
+module.exports.hidden = true
 
-                return `\`\`\`js\n${result}\n\`\`\``;
-            } catch (err) {
-                return `**ERROR** \`\`\`xl\n${err}\n\`\`\``;
-            }
+module.exports.execute = async function (interaction) {
+    if (interaction.member.user.id === dev) {
+        try {
+            const user = await UserDB.getUserById(interaction.member.user.id);
+            const result = eval(interaction.data.options[0].value);
+
+            return `\`\`\`js\n${result}\n\`\`\``;
+        } catch (err) {
+            return `**ERROR** \`\`\`xl\n${err}\n\`\`\``;
         }
-    },
-    options: [
-        {
-            name: "code",
-            description: "code to evaluate",
-            type: 3,
-            required: true
-        }
-    ]
+    }
 }
+
+module.exports.options = [
+    {
+        name: "code",
+        description: "code to evaluate",
+        type: 3,
+        required: true
+    }
+]
--- commands/dev/unban.js
+++ commands/dev/unban.js
@@ -1,33 +1,33 @@
 const fs = require("fs");
 const { dev } = require("../../config/discordIds.json");
 
-module.exports = {
-    name: "unban",
-    description: "Unbans user from the bot",
-    syntax: "`/unban`",
-    hidden: true,
-    async execute(interaction) {
-        if (interaction.member.user.id === dev) return;
+module.exports.name = "unban"
+module.exports.description = "Unbans user from the bot"
+module.exports.syntax = "`/unban`"
+module.exports.hidden = true
 
-        const userID = interaction.data.options[0].value;
-        const bannedUsers = JSON.parse(fs.readFileSync("./config/bannedUsers.json"));
+module.exports.execute = async function (interaction) {
+    if (interaction.member.user.id === dev) return;
 
-        const index = bannedUsers.indexOf(userID);
-        if (index === -1) return "User not found";
-        bannedUsers.splice(index, 1);
-        fs.writeFileSync(
-            "./config/bannedUsers.json",
-            JSON.stringify(bannedUsers, null, 4)
-        );
+    const userID = interaction.data.options[0].value;
+    const bannedUsers = JSON.parse(fs.readFileSync("./config/bannedUsers.json"));
 
-        return `\`${userID}\` unbanned`;
-    },
-    options: [
-        {
-            name: "id",
-            description: "user ID",
-            type: 3,
-            required: true
-        }
-    ]
+    const index = bannedUsers.indexOf(userID);
+    if (index === -1) return "User not found";
+    bannedUsers.splice(index, 1);
+    fs.writeFileSync(
+        "./config/bannedUsers.json",
+        JSON.stringify(bannedUsers, null, 4)
+    );
+
+    return `\`${userID}\` unbanned`;
 }
+
+module.exports.options = [
+    {
+        name: "id",
+        description: "user ID",
+        type: 3,
+        required: true
+    }
+]
