824de9bbfad839174713bca299f901117606d78c
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-07 22:05:37 -0400

    Format scanners

--- scanners/claimall.js
+++ scanners/claimall.js
@@ -1,19 +1,18 @@
-module.exports = {
-    name: "claimall",
-    execute: async function(interaction, userID) {
-        const newMessage = {
-            author: {
-                id: userID
-            },
-            channel: interaction.channel
-        };
+module.exports.name = "claimall"
 
-        for (kit of interaction.embeds[0].description.split("\n")) {
-            const name = kit.split("**")[1].toLowerCase();
+module.exports.execute = async function (interaction, userID) {
+    const newMessage = {
+        author: {
+            id: userID
+        },
+        channel: interaction.channel
+    };
 
-            try {
-                await bot.timers.get(name).execute(newMessage, userID);
-            } catch (error) {}
-        }
+    for (kit of interaction.embeds[0].description.split("\n")) {
+        const name = kit.split("**")[1].toLowerCase();
+
+        try {
+            await bot.timers.get(name).execute(newMessage, userID);
+        } catch (error) { }
     }
 }
--- scanners/petScan.js
+++ scanners/petScan.js
@@ -1,57 +1,56 @@
 const UserDB = require("../database/controllers/userController");
 
-module.exports = {
-    name: "petScan",
-    execute: async function (embed, userID) {
-        const user = await UserDB.getUserById(userID);
-        user.pets.shards = Number(embed.description.split("\n")[2].split(" ")[2].replace(/,/g, ''));
+module.exports.name = "petScan"
 
-        for (const pet of Object.keys(user.pets)) {
-            if (pet !== "shards") {
-                user.pets[pet] = 0;
-            }
+module.exports.execute = async function (embed, userID) {
+    const user = await UserDB.getUserById(userID);
+    user.pets.shards = Number(embed.description.split("\n")[2].split(" ")[2].replace(/,/g, ''));
+
+    for (const pet of Object.keys(user.pets)) {
+        if (pet !== "shards") {
+            user.pets[pet] = 0;
         }
-
-        embed.fields && embed.fields.forEach(field => {
-            const rarity = field.name.trim().split("**")[1].toLowerCase();
-            const pets = field.value.trim().split("\n");
-
-            if (["common", "uncommon", "rare", "epic"].includes(rarity)) {
-                pets.forEach(pet => {
-                    if (!pet.includes("Max. level:")) {
-                        const petLevel = pet.includes("★")
-                            ? Number(pet.split(" ")[4].replace("]", '').trim())
-                            : Number(pet.split(" ")[3].replace("]", '').trim());
-
-                        const db = (pet.includes("★") && `★ ${rarity}`) || rarity;
-                        user.pets[db] += petLevel;
-                    }
-                });
-            } else if (rarity === "mythical") {
-                pets.forEach(pet => {
-                    if (!pet.includes("Max. level:")) {
-                        const petName = (pet.split(" ")[(pet.includes("★") && 2) || 1].split("**")[1].trim()).toLowerCase();
-                        const petLevel = Number(pet.split(" ")[(pet.includes("★") && 4) || 3].replace("]", '').trim()) * ((pet.includes("★") && 2) || 1);
-
-                        if (user.pets[petName] >= 0) {
-                            user.pets[petName] = petLevel;
-                        }
-                    }
-                });
-            } else if (rarity === "legendary") {
-                pets.forEach(pet => {
-                    if (!pet.includes("Max. level:")) {
-                        const petName = (pet.split(" ")[(pet.includes("★") && 2) || 1].split("**")[1].trim()).toLowerCase();
-                        const petLevel = Number(pet.split(" ")[(pet.includes("★") && 4) || 3].replace("]", '').trim());
-
-                        if (user.pets[petName] >= 0) {
-                            user.pets[petName] = petLevel;
-                        }
-                    }
-                });
-            }
-        });
-
-        await user.save();
     }
+
+    embed.fields && embed.fields.forEach(field => {
+        const rarity = field.name.trim().split("**")[1].toLowerCase();
+        const pets = field.value.trim().split("\n");
+
+        if (["common", "uncommon", "rare", "epic"].includes(rarity)) {
+            pets.forEach(pet => {
+                if (!pet.includes("Max. level:")) {
+                    const petLevel = pet.includes("★")
+                        ? Number(pet.split(" ")[4].replace("]", '').trim())
+                        : Number(pet.split(" ")[3].replace("]", '').trim());
+
+                    const db = (pet.includes("★") && `★ ${rarity}`) || rarity;
+                    user.pets[db] += petLevel;
+                }
+            });
+        } else if (rarity === "mythical") {
+            pets.forEach(pet => {
+                if (!pet.includes("Max. level:")) {
+                    const petName = (pet.split(" ")[(pet.includes("★") && 2) || 1].split("**")[1].trim()).toLowerCase();
+                    const petLevel = Number(pet.split(" ")[(pet.includes("★") && 4) || 3].replace("]", '').trim()) * ((pet.includes("★") && 2) || 1);
+
+                    if (user.pets[petName] >= 0) {
+                        user.pets[petName] = petLevel;
+                    }
+                }
+            });
+        } else if (rarity === "legendary") {
+            pets.forEach(pet => {
+                if (!pet.includes("Max. level:")) {
+                    const petName = (pet.split(" ")[(pet.includes("★") && 2) || 1].split("**")[1].trim()).toLowerCase();
+                    const petLevel = Number(pet.split(" ")[(pet.includes("★") && 4) || 3].replace("]", '').trim());
+
+                    if (user.pets[petName] >= 0) {
+                        user.pets[petName] = petLevel;
+                    }
+                }
+            });
+        }
+    });
+
+    await user.save();
 }
