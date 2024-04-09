8110e6a9c675bd6f5d08665f30139c93b31e3b6d
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-09 12:46:16 -0400

    Remove stats poster

--- main.js
+++ main.js
@@ -17,14 +17,6 @@ global.bot = rbr;
 
 process.on("uncaughtException", (error) => {
     rbr.error("Uncaught Exception", error);
-})
-
-// Top.gg
-const { AutoPoster } = require("topgg-autoposter");
-const poster = AutoPoster(process.env.TOPGG_TOKEN, rbr);
-
-poster.on('posted', () => {
-    console.info('Posted stats to Top.gg!');
 });
 
 // Deals with any rejected Promises that haven't been handled
