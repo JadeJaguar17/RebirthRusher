c645f35bd03fe19e78f121b5233d4f95739fd981
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-09 13:08:05 -0400

    Add comments to main

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const RebirthRusher = require("./system/RebirthRusher");
 require("dotenv").config();
 
 const AUTH_TOKEN = process.env.NODE_ENV === "development"
-? process.env.DEVTOKEN
-: TOKEN;
+    ? process.env.DEVTOKEN
+    : TOKEN;
 
+// log which version of bot is being run
 if (AUTH_TOKEN === process.env.DEVTOKEN) {
     console.info("Starting up dev bot");
 }
@@ -16,21 +17,24 @@ else {
     exit();
 }
 
+// create RebirthRusher bot and start it up
 const rbr = new RebirthRusher(AUTH_TOKEN);
 rbr.init();
 
+global.bot = rbr;
+
+// handle errors
 rbr.on("error", error => {
     rbr.error("Error event", error);
     console.error(error)
 });
 
-global.bot = rbr;
-
+// handle any uncaught exceptions
 process.on("uncaughtException", (error) => {
     rbr.error("Uncaught Exception", error);
 });
 
-// Deals with any rejected Promises that haven't been handled
+// deals with any undhandled rejected Promises
 process.on('unhandledRejection', error => {
     rbr.error("Unhandled Promise Rejection", error);
 });
