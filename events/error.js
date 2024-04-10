a9fab93164022c10d6ae5dc1094998b84587c5f1
JadeJagar17 <thejadejaguar17@gmail.com>
2024-04-10 09:52:50 -0400

    Add note about single-shard bot autoconnect

--- events/error.js
+++ events/error.js
@@ -8,3 +8,8 @@ module.exports = async (bot, error, _) => {
     console.error(error);
     bot.error("Error Event", error);
 }
+
+// Apparentlly there's been a long-running issue in Eris where single-shard bots
+// can't reconnect, even if the error event is being handled. The devs have no
+// idea why it's happening, so it's unlikely a fix will be out anytime soon. In
+// the meantime, we'll just leave this code here
