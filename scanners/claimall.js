c38f1b879dc381662121c8c4b53b7be956b543fd
tikkikkit21 <tikkikkit21@gmail.com>
2025-02-11 19:31:49 -0500

    Update scanners

--- scanners/claimall.js
+++ scanners/claimall.js
@@ -1,14 +1,25 @@
+/**
+ * @typedef {import("../RebirthRusher.js")} RebirthRusher
+ * @typedef {import("eris").Message} Message
+ */
+
 module.exports.name = "claimall"
 
-module.exports.execute = async function (interaction, userID) {
+/**
+ * Scans claimed kits from Idle Miner /claimall
+ * @param {RebirthRusher} bot instance of RbR base class
+ * @param {Message} message triggering Eris interaction
+ * @param {string} userID user's Discord ID
+ */
+module.exports.execute = async function (bot, message, userID) {
     const newMessage = {
         author: {
             id: userID
         },
-        channel: interaction.channel
+        channel: message.channel
     };
 
-    for (kit of interaction.embeds[0].description.split("\n")) {
+    for (const kit of message.embeds[0].description.split("\n")) {
         const name = kit.split("**")[1].toLowerCase();
 
         try {
